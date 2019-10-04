e9030fe490de79229b09cfef688aa18de51396ea
KevinAS28 <kevinagusto28@gmail.com>
2019-10-04 16:47:57 +0700

    Judger login fix 1

--- ProgrammingClass/settings.py
+++ ProgrammingClass/settings.py
@@ -57,7 +57,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, "GetStudent", "template")
+            os.path.join(BASE_DIR, "GetStudent", "template"),
+            os.path.join(BASE_DIR, "Judger", "template")
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -135,3 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
--- ProgrammingClass/urls.py
+++ ProgrammingClass/urls.py
@@ -19,5 +19,6 @@ from django.urls import include
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path("getstudent/", include('GetStudent.urls', namespace='get_student') ),
+    path("judger/", include('Judger.urls', namespace='judger') ),
     
 ]
